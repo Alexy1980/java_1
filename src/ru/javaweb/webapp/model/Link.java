05beaef7a50841dc621f5908c101e2493b82be91
Paul <test@mail.ru>
2017-10-02 16:57:50 +0400

    end Link

---
+++ src/ru/javaweb/webapp/model/Link.java
@@ -0,0 +1,8 @@
+package ru.javaweb.webapp.model;
+
+public class Link {
+    private String name;
+    private String url;
+
+}
+
--- src/ru/javaweb/webapp/model/Resume.java
+++ src/ru/javaweb/webapp/model/Resume.java
@@ -3,7 +3,6 @@ package ru.javaweb.webapp.model;
 import java.util.Collection;
 
 public class Resume {
-    String firstName;
-    String[] some;
-    Collection<String> collection;
+    private String fullName;
+    private String location;
 }
