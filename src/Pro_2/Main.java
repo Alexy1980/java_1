19fb222473b89d25d20431469086ba1b0bcd8332
Paul <test@mail.ru>
2017-10-02 21:38:37 +0400

    equals переопределен

--- src/Pro_2/Main.java
+++ src/Pro_2/Main.java
@@ -1,5 +1,17 @@
 package Pro_2;
 
-public class Main {
+import ru.javaweb.webapp.model.Link;
 
+public class Main {
+    public static void main(String[] args){
+        // обращаемся к конструктору
+        Link l1 = new Link("test", "test.ru");
+        Link l2 = l1;
+        Link l3 = new Link(l2);
+        // вывод полного пути класса l1
+        System.out.println(l1.getClass());
+        // проверим равенство объектов
+        System.out.println(l1.equals(l2));
+        System.out.println(l1.equals(l3));
+    }
 }
--- src/ru/javaweb/webapp/model/Link.java
+++ src/ru/javaweb/webapp/model/Link.java
@@ -2,12 +2,31 @@ package ru.javaweb.webapp.model;
 
 public class Link {
     // final означает, что свойство не меняется
-    private String name = "name";
-    private String url = "url";
+    private final String name;
+    private final String url;
     // конструктор
     public Link(String name, String url) {
        this.name = name;
        this.url = url;
     }
-}
+    // делаем копию Link (клонируем)
+    public Link(Link link) {
+        this.name = link.name;
+        this.url = link.url;
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Link link = (Link) o;
+
+        return url != null ? url.equals(link.url) : link.url == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return url != null ? url.hashCode() : 0;
+    }
+}
